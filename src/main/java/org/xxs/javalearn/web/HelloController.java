7e049ca74f0d0c4490cd48ab2916bf049f1330bc
LandMrakYao <763315937@qq.com>
2018-12-11 20:53:20 +0800

    test method add hello

--- src/main/java/org/xxs/javalearn/web/HelloController.java
+++ src/main/java/org/xxs/javalearn/web/HelloController.java
@@ -1,17 +1,13 @@
 package org.xxs.javalearn.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/test")
     public String test(String id){
-
-
-
-        return "test";
+        return "hello";
     }
-
 }

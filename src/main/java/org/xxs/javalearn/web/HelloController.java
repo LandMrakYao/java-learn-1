668a4a2d09c996443d5c6535d73716dba54b211d
LandMrakYao <763315937@qq.com>
2018-12-11 18:55:30 +0800

    test method add annotation...

--- src/main/java/org/xxs/javalearn/web/HelloController.java
+++ src/main/java/org/xxs/javalearn/web/HelloController.java
@@ -1,11 +1,16 @@
 package org.xxs.javalearn.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/test")
     public String test(){
+
+
+
         return "test";
     }
 
